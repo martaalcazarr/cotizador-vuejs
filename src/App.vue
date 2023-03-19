91cb1402e746a640772b041cba60a73cb9c647ec
martaalcazarr <martaalcazarredondo@gmail.com>
2023-03-19 19:26:41 -0300

    input para rango de dinero

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <script setup>
+  import {ref} from 'vue'
   import Header from './components/Header.vue'
+
+  const cantidad = ref(2500000)
+  const MIN = 0
+  const MAX = 5000000
+  const STEP = 100000
+ 
+
+
+
+  
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+    <div class="my-5">
+      <input 
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        :min="MIN"
+        :max="MAX"
+        :step="STEP"
+        v-model.number="cantidad"
+        
+        />
+        <p>{{ cantidad }}</p>
+        
+    </div>
   </div>
  
 </template>
